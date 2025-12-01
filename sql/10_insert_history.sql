e85e58dcd278de27decf3e161fc92e4ca839fa21
Glaucia Maria <glauciamariagnf0@gmail.com>
2025-12-01 18:20:46 -0300

    Delete sql/10_insert_history.sql

--- sql/10_insert_history.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO history (history_id, user_id, track_id, listened_at, device, duration_listened_seconds) VALUES
-(1, 1, 1, '2025-02-10 10:00:00', 'Android', 180),
-(2, 1, 5, '2025-02-10 10:05:00', 'PC', 248),
-(3, 2, 7, '2025-02-11 08:30:00', 'iPhone', 200),
-(4, 2, 3, '2025-02-11 09:00:00', 'iPad', 210),
-(5, 3, 9, '2025-02-12 22:15:00', 'Android', 260),
-(6, 3, 10, '2025-02-12 22:25:00', 'Android', 300);
