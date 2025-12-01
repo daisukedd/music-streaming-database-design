97702ba138b19f650aa848d45b11f9bb353030ac
Glaucia Maria <glauciamariagnf0@gmail.com>
2025-12-01 18:19:44 -0300

    Delete sql/04_insert_albums.sql

--- sql/04_insert_albums.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO album (album_id, artist_id, title, release_year, label) VALUES
-(1, 1, 'After Hours', 2020, 'XO Records'),
-(2, 2, '1989', 2014, 'Republic Records'),
-(3, 3, 'Random Access Memories', 2013, 'Columbia Records'),
-(4, 4, 'Happier Than Ever', 2021, 'Interscope'),
-(5, 5, 'The Marshall Mathers LP', 2000, 'Aftermath Entertainment');
