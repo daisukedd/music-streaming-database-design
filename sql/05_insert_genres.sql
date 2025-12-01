513fd89d75f7140b4b76e4f6a2484039e7def35e
Glaucia Maria <glauciamariagnf0@gmail.com>
2025-12-01 18:19:55 -0300

    Delete sql/05_insert_genres.sql

--- sql/05_insert_genres.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO genre (genre_id, name, description) VALUES
-(1, 'Pop', 'Música popular contemporânea.'),
-(2, 'Hip Hop', 'Rap e cultura urbana.'),
-(3, 'Electronic', 'Música eletrônica e house.'),
-(4, 'Alternative', 'Pop alternativo e experimental.'),
-(5, 'R&B', 'Rhythm and Blues moderno.');
